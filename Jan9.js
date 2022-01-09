adb6d621d4a61f7e57ebbdf29dbb73705244765f
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-01-09 15:09:20 -0600

    Add files via upload

---
+++ Jan9.js
@@ -0,0 +1,46 @@
+// 1:05 - 3:05
+// video 41 -  43
+
+const yasmineArray = [
+    'Yasmine',
+    'Abdilahi',
+    2037 - 2010,
+    'Student'
+    ['Luna', 'Suwaydah', 'Hamdi']
+];
+
+const Yasmine = {
+    firstName: 'Yasmine',
+    lastName: 'Abdilahi',
+    age: 2037 - 2010,
+    job: 'student',
+    friends: ['luna', 'suwaydah', 'Hamdi']
+};
+
+////////////////////////////////////////////////
+
+const Yasmine = {
+    firstName: 'Yasmine',
+    lastName: 'Abdilahi',
+    age: 2037 - 2010,
+    job: 'student',
+    friends: ['luna', 'suwaydah', 'Hamdi']
+};
+console.log(Yasmine);
+
+console.log(Yasmine.lastName);
+console.log(Yasmine['lastName']);
+
+const nameKey = 'Name'
+console.log(Yasmine['first' + nameKey]);
+console.log(Yasmine['last' + nameKey]);
+
+
+
+const interestIn = prompt('CATS CATS CATS!!!');
+
+if (Yasmine[interestedIn]) {
+    console.log(Yasmine[intrestedIn]);
+} else {
+    console.log('....')
+}
